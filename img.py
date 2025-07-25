903655ae6d106a3cbde23a14c13115f93b43804a
Prashanthcoder <tp120113@gmail.com>
2025-07-25 16:15:07 +0530

    First commit

---
+++ img.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import random
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+def dummy_classifier(image_bytes):
+    labels = ['pizza', 'burger', 'idli', 'rice', 'dosa', 'superman', 'teacher']
+    return random.choice(labels)
+
+
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    print(f"file recieved {file.filename}")
+    prediction = dummy_classifier(contents)
+    return {"filename": file.filename,
+            "size_kb": len(contents)//1024,
+            "prediction": prediction}
